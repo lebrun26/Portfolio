16125d8a2b64d693213a507aeab207db2fe203e0
lebrun26 <lebrun.patrickdev@gmail.com>
2024-09-18 10:51:11 +0200

    Utilisation des composants header et footer sur les différentes pages

--- src/pages/Presentation.jsx
+++ src/pages/Presentation.jsx
@@ -1,8 +1,22 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const Presentation = () => {
-  return <div> test présentation</div>;
+  return (
+    <div className="wrapper">
+      <header>
+        <Header />
+      </header>
+
+      <main className="main__container">{/* Ton contenu ici */}</main>
+
+      <footer className="footer">
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default Presentation;
--- src/pages/Projets.jsx
+++ src/pages/Projets.jsx
@@ -1,8 +1,22 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const Projets = () => {
-  return <div>test projets</div>;
+  return (
+    <div className="wrapper">
+      <header>
+        <Header />
+      </header>
+
+      <main className="main__container">{/* Ton contenu ici */}</main>
+
+      <footer className="footer">
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default Projets;
