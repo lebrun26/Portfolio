cb2b176db79659802dd668ac9ec73d4761776029
lebrun26 <lebrun.patrickdev@gmail.com>
2024-09-21 09:46:07 +0200

    Mise en place des cards correspondant au tech maitrisé

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import "./Card.scss"; // Importation des styles
 
+// eslint-disable-next-line react/prop-types
 const Card = ({ title, logo }) => {
   return (
     <div className="card">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,12 @@ import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Card from "../components/Card";
+import logoHtml from "../assets/logo_html.png";
+import logoCss from "../assets/logo_css.png";
+import logoSass from "../assets/logo_sass.png";
+import logoJs from "../assets/logo_js.png";
+import logoReact from "../assets/logo_react.png";
+import logoRedux from "../assets/logo_redux.png";
 
 const Home = () => {
   const [animate, setAnimate] = useState(false);
@@ -29,7 +35,12 @@ const Home = () => {
           ma formation.
         </p>
         <div className="container__techno">
-          <Card title="HTML" logo="logoHtml" />
+          <Card title="HTML" logo={logoHtml} />
+          <Card title="CSS" logo={logoCss} />
+          <Card title="SASS" logo={logoSass} />
+          <Card title="JavaScript" logo={logoJs} />
+          <Card title="React" logo={logoReact} />
+          <Card title="Redux" logo={logoRedux} />
         </div>
       </main>
       <footer className="footer">
