4c0df00d7ee77b38bdd8f8e5ab218accc7e1609f
lebrun26 <lebrun.patrickdev@gmail.com>
2024-09-22 13:23:15 +0200

    Mise en place des cards pour la partie présentation du site + animation sur cards

---
+++ src/components/Cards_presentation.jsx
@@ -0,0 +1,30 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from "react";
+
+const Cards_presentation = ({ title, text }) => {
+  const [flipped, setFlipped] = useState(false);
+
+  const handleMouseEnter = () => setFlipped(true);
+  const handleMouseLeave = () => setFlipped(false);
+
+  return (
+    <div
+      className="card__presentation"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div className={`card__presentation__inner ${flipped ? "flipped" : ""}`}>
+        {/* Face avant de la carte */}
+        <div className="card__presentation__inner__front">
+          <h2 className="title__cards">{title}</h2>
+        </div>
+        {/* Face arrière de la carte */}
+        <div className="card__presentation__inner__back">
+          <div>{text}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cards_presentation;
--- src/pages/Presentation.jsx
+++ src/pages/Presentation.jsx
@@ -1,7 +1,9 @@
+/* eslint-disable react/no-unescaped-entities */
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Cards_presentation from "../components/Cards_presentation";
 
 const Presentation = () => {
   return (
@@ -12,54 +14,117 @@ const Presentation = () => {
 
       <main className="main__container">
         <h2 className="main__container__title">Présentation</h2>
+        {/* Parcours */}
+        <Cards_presentation
+          title="Mon parcours"
+          text={
+            <div className="main__container__parcours">
+              <p className="main__container__parcours__paragraphe">
+                <span className="main__container__parcours__paragraphe__style__date">
+                  Juillet 2016 :
+                </span>{" "}
+                Obtention du Bac Pro Maintenance des équipements industriels
+              </p>
+              <p className="main__container__parcours__paragraphe">
+                <span className="main__container__parcours__paragraphe__style__date">
+                  Janvier 2017 :
+                </span>{" "}
+                Élève Sous-Officier de la Marine Nationale
+              </p>
+              <p className="main__container__parcours__paragraphe">
+                <span className="main__container__parcours__paragraphe__style__date">
+                  Septembre 2018 :
+                </span>{" "}
+                Étudiant - Sciences et techniques des activités physiques et
+                sportives
+              </p>
+              <p className="main__container__parcours__paragraphe">
+                <span className="main__container__parcours__paragraphe__style__date">
+                  Septembre 2019 :
+                </span>{" "}
+                Assistant d'éducation dans un collège, lycée
+              </p>
+              <p className="main__container__parcours__paragraphe">
+                <span className="main__container__parcours__paragraphe__style__date">
+                  Juillet 2021 :
+                </span>{" "}
+                Chauffeur / Livreur dans le transport de colis pour les
+                particuliers
+              </p>
+              <p className="main__container__parcours__paragraphe">
+                <span className="main__container__parcours__paragraphe__style__date">
+                  Août 2023 :
+                </span>{" "}
+                Chauffeur / Livreur dans le transport de produits d'entretien
+                pour les professionnels
+              </p>
+              <p className="main__container__parcours__paragraphe">
+                <span className="main__container__parcours__paragraphe__style__date">
+                  Décembre 2023 :
+                </span>{" "}
+                Début de formation Intégrateur Web chez OpenClassRooms
+              </p>
+            </div>
+          }
+        />
+        {/* Formation OpenClassRooms */}
+        <Cards_presentation
+          title="Ma formation"
+          text={
+            <div className="main__container__formation">
+              <p className="main__container__formation__paragraphe">
+                Ma formation d'intégrateur web chez OpenClassRooms a été très
+                instructive. Avec de petites bases uniquement en HTML et CSS,
+                grâce à cette formation, j'ai réussi à atteindre le premier
+                niveau d'exigence et d'apprentissage que je souhaitais.{" "}
+              </p>
+              <p className="main__container__formation__paragraphe">
+                {" "}
+                La première partie de la formation nous a appris le langage HTML
+                et CSS. Suite à cela, j'ai fait mes premiers pas sur du
+                JavaScript pure. Ensuite j'ai appris ma première bibliothèque,
+                le "React".
+              </p>
+              <p className="main__container__formation__paragraphe">
+                C'est à ce moment précis que j'ai vraiment pris du plaisir à
+                développer. Et la dernière chose apprise, "Redux" outils très
+                importants et surtout très puissants. Dans cette formation, ce
+                qu'il y a de bien, c'est le fait d'avoir vraiment des projets
+                qui nous professionnalisent, nous mettent vraiment en conditions
+                pour nos futures entreprises.{" "}
+              </p>
+              <p className="main__container__formation__paragraphe">
+                {" "}
+                J'ai eu aussi l'occasion d'être formé sur l'utilisation de Figma
+                et la gestion de projet avec la méthode "Agile".
+              </p>
+            </div>
+          }
+        />
 
-        <div className="main__container__parcours">
-          <h3 className="main__container__parcours__title">Mon parcours :</h3>
-          <p className="main__container__parcours__paragraphe">
-            <span className="main__container__parcours__paragraphe__style__date">
-              Juillet 2016 :
-            </span>{" "}
-            Obtention du Bac Pro Maintenance des équipements industriels
-          </p>
-          <p className="main__container__parcours__paragraphe">
-            <span className="main__container__parcours__paragraphe__style__date">
-              Janvier 2017 :
-            </span>{" "}
-            Élève Sous-Officier de la Marine Nationale
-          </p>
-          <p>
-            <span className="main__container__parcours__paragraphe__style__date">
-              Septembre 2018 :
-            </span>{" "}
-            Étudiant - Sciences et techniques des activités physiques et
-            sportives
-          </p>
-          <p className="main__container__parcours__paragraphe">
-            <span className="main__container__parcours__paragraphe__style__date">
-              Septembre 2019 :
-            </span>{" "}
-            Assistant d'éducation dans un collège, lycée
-          </p>
-          <p className="main__container__parcours__paragraphe">
-            <span className="main__container__parcours__paragraphe__style__date">
-              Juillet 2021 :
-            </span>{" "}
-            Chauffeur / Livreur dans le transport de colis pour les particuliers
-          </p>
-          <p className="main__container__parcours__paragraphe">
-            <span className="main__container__parcours__paragraphe__style__date">
-              Août 2023 :
-            </span>{" "}
-            Chauffeur / Livreur dans le transport de produits d'entretien pour
-            les professionnels
-          </p>
-          <p className="main__container__parcours__paragraphe">
-            <span className="main__container__parcours__paragraphe__style__date">
-              Décembre 2023 :
-            </span>{" "}
-            Début de formation Intégrateur Web chez OpenClassRooms
-          </p>
-        </div>
+        {/* Mon future */}
+        <Cards_presentation
+          title="Mon future"
+          text={
+            <div className="main__container__future">
+              <p className="main__container__future__paragraphe">
+                Pour mon futur, mon objectif final est de devenir "Développeur
+                en intelligence artificielle".{" "}
+              </p>{" "}
+              <p className="main__container__future__paragraphe">
+                {" "}
+                C'est pour cela que, suite à l'obtention de mon diplôme,
+                j'aimerais trouver une alternance en tant que développeur full
+                stack avec du Pyton en backend.{" "}
+              </p>{" "}
+              <p className="main__container__future__paragraphe">
+                {" "}
+                Afin d'avoir une base au niveau du langage de programmation pour
+                les IA.
+              </p>
+            </div>
+          }
+        />
       </main>
 
       <footer className="footer">
