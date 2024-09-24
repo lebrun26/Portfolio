4bfc177bbd580ae5c6e4804c8edbb253b7cb1f40
lebrun26 <lebrun.patrickdev@gmail.com>
2024-09-24 15:07:15 +0200

    Création menu burger pour responsive phone + responsive page home + responsive 100% composant header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,42 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="Header__container">
       <NavLink to="/">
         <h1>Patrick Le Brun</h1>
       </NavLink>
 
-      <nav>
-        <NavLink to="/">
+      {/* Bouton Burger */}
+      <div
+        className={`burger__menu ${isOpen ? "open" : ""}`}
+        onClick={toggleMenu}
+      >
+        <div className="bar1"></div>
+        <div className="bar2"></div>
+        <div className="bar3"></div>
+      </div>
+
+      {/* Menu */}
+      <nav className={`nav-menu ${isOpen ? "open" : ""}`}>
+        <NavLink to="/" onClick={toggleMenu}>
           <span>Accueil</span>
         </NavLink>
-        <NavLink to="/presentation">
+        <NavLink to="/presentation" onClick={toggleMenu}>
           <span>Présentation</span>
         </NavLink>
-        <NavLink to="/projets">
+        <NavLink to="/projets" onClick={toggleMenu}>
           <span>Projets</span>
         </NavLink>
-        <NavLink to="/contact">
+        <NavLink to="/contact" onClick={toggleMenu}>
           <span>Contact</span>
         </NavLink>
       </nav>
